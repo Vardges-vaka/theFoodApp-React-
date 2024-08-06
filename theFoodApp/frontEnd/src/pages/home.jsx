7293f8d9293465b0355898e00a2d66e994062417
Marwa Jawad <marwajawad@Marwas-MacBook-Pro.local>
2024-08-06 10:57:07 +0400

    new branch

--- theFoodApp/frontEnd/src/pages/home.jsx
+++ theFoodApp/frontEnd/src/pages/home.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState, Profiler } from "react";
 import "./styling/home.css";
 import Header from "./sharedComponents/header";
 import Footer from "./sharedComponents/footer";
 import CopyRight from "./sharedComponents/copyRight";
 import AppSection from "./sharedComponents/appSection";
-
+import useFoodList from "../hooks/foodList";
 function Home() {
+  const { data, isLoading } = useFoodList();
+  console.log(data, isLoading);
+
+  const callOnRender = (id, phase, actualDuration, startTime) => {
+    console.log(id, phase, actualDuration, startTime, "profile");
+  };
+
   return (
     <>
-      {" "}
-      <Header />
-      <div>
-        <h1 className="placeholder">Home</h1>
-      </div>
-      <AppSection />
-      <Footer />
-      <CopyRight />
+      <Profiler id="home" onRender={callOnRender}>
+        {!isLoading && data.map((food) => <div>{food.name}</div>)}
+        <Header />
+        <AppSection />
+        <Footer />
+        <CopyRight />
+      </Profiler>
     </>
   );
 }
